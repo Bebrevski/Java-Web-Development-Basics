0f9cce5497e5a92a813fa0fbe11ba46510618fce
Bebrevski <stefan.bebrevski89@gmail.com>
2019-02-20 21:17:15 +0200

    WORKSHOP: JAVA EE: JSF

---
+++ 10. WORKSHOP JAVA EE JSF + JPA/src/main/java/panda/domain/entity/Status.java
@@ -0,0 +1,5 @@
+package panda.domain.entity;
+
+public enum Status {
+    Pending, Shipped, Delivered, Acquired
+}
---
+++ 10. WORKSHOP JAVA EE JSF + JPA/src/main/java/panda/service/UserService.java
@@ -0,0 +1,9 @@
+package panda.service;
+
+import panda.domain.models.service.UserServiceModel;
+
+public interface UserService {
+    void userRegister(UserServiceModel userServiceModel);
+
+    UserServiceModel userLogin(UserServiceModel userServiceModel);
+}

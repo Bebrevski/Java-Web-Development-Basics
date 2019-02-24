a6749fb1fb2d9529fa69485a962c83b4125270de
Bebrevski <stefan.bebrevski89@gmail.com>
2019-02-24 19:13:19 +0200

    Fix problem

--- 12. My Exam/src/main/java/app/web/beans/DocumentPrintBean.java
+++ 12. My Exam/src/main/java/app/web/beans/DocumentPrintBean.java
@@ -24,12 +24,31 @@ public class DocumentPrintBean extends BaseBean {
     public DocumentPrintBean(DocumentService documentService, ModelMapper modelMapper) {
         this.documentService = documentService;
         this.modelMapper = modelMapper;
-        this.init();
     }
 
     @PostConstruct
     private void init() {
         this.model = new DocumentViewModel();
+
+        var sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+        String docId = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
+                .getParameter("docId");
+
+        if (docId != null) {
+
+            this.model = this.modelMapper.map(this.documentService.findById(docId), DocumentViewModel.class);
+
+            sessionMap.put("docId", this.model.getId());
+
+        } else {
+            String savedId = (String) FacesContext.getCurrentInstance().getExternalContext()
+                    .getSessionMap().get("docId");
+
+            this.model = this.modelMapper.map(this.documentService.findById(savedId), DocumentViewModel.class);
+
+            sessionMap.remove("docId");
+        }
     }
 
     public DocumentViewModel getDoc(String id) {
@@ -38,8 +57,7 @@ public class DocumentPrintBean extends BaseBean {
     }
 
     public void print() {
-        String id = ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest()).getParameter("docId");
-        this.documentService.delete(id);
+        this.documentService.delete(this.model.getId());
         this.redirect("home");
     }
 }
